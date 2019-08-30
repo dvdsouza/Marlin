a613b9f7b0bb69cf336799235d1bb8fb24a92652
David Souza <dvdcmd@gmail.com>
2019-08-30 20:15:53 -0300

    hotend pid settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -364,7 +364,7 @@
 
 // Comment the following line to disable PID and enable bang-bang.
 #define PIDTEMP
-#define BANG_MAX 255     // Limits current to nozzle while in bang-bang mode; 255=full current
+#define BANG_MAX 180     // Limits current to nozzle while in bang-bang mode; 255=full current
 #define PID_MAX BANG_MAX // Limits current to nozzle while PID is active (see PID_FUNCTIONAL_RANGE below); 255=full current
 #define PID_K1 0.95      // Smoothing factor within any PID loop
 #if ENABLED(PIDTEMP)
@@ -380,9 +380,9 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Ultimaker
-  #define DEFAULT_Kp 22.2
-  #define DEFAULT_Ki 1.08
-  #define DEFAULT_Kd 114
+  #define DEFAULT_Kp 26.79
+  #define DEFAULT_Ki 2.89
+  #define DEFAULT_Kd 62.00
 
   // MakerGear
   //#define DEFAULT_Kp 7.0
