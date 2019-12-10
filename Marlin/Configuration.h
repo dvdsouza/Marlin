efa3078babeda11c307f4c70c6fea26a79e1b33a
David Souza <dvdcmd@gmail.com>
2019-12-10 19:14:24 -0300

    Enable the Print Counter

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1560,7 +1560,7 @@
  *
  * View the current statistics with M78.
  */
-//#define PRINTCOUNTER
+#define PRINTCOUNTER
 
 //=============================================================================
 //============================= LCD and SD support ============================
